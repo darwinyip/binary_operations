8457f75bc33f4f2f1bc59b75f6a1ff833af353ad
darwinyip <darwinyip@users.noreply.github.com>
2017-02-13 23:31:17 -0500

    Added iterative sum, renamed sum to recursive

--- python/sum.py
+++ python/sum.py
@@ -1,7 +1,15 @@
-def sum(a, b):
+def recursive_sum(a, b):
 	if(b != 0):
 		s = (a ^ b)
 		c = (a & b) << 1
 		return sum(s, c)
 	else:
 		return a
+
+
+def iterative_sum(a, b):
+	while(b != 0):
+		s = (a ^ b)
+		b = (a & b) << 1
+		a = s
+	return a
